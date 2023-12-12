41e369076d4daf8f47de587067a7c66afce3aa7c
Andrea Paradiso <147241527+AndreaP23@users.noreply.github.com>
2023-12-12 20:54:41 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const axios = require("axios");
 const express = require("express");
 const path = require("path");
 const userModel= require("./config").userModel;
 const tripModel= require("./config").tripModel;
+const reviewModel= require("./config").reviewModel;
 const bcrypt = require('bcrypt');
-const islandsData= require("/Users/andreaparadiso/Desktop/Progetto_CI/api/islands.js");
+const islandsData= require("../api/islands.js");
+const mongoose = require('mongoose');
+
 
 const app = express();
 // convert data into json format
@@ -32,9 +36,6 @@ app.get("/signup", (req, res) => {
 app.get("/home",(req,res)=> {
     res.render("home");
 })
-app.get("/complete", (req, res) => {
-    res.render("completetrip");
-});
 
 app.get("/choosetrip", (req, res) => {
     res.render("choosetrip", { islandsData });
@@ -102,8 +103,7 @@ app.post("/login", async (req, res) => {
         if (!isPasswordMatch) {
             return res.render("login", { error: 'incorrectPassword' });
         }
-
-        res.render("home");
+        res.render("auth", { email: req.body.email, logout: false });
     } catch (error) {
         console.error('Error during login:', error);
         res.status(500).send('Internal Server Error');
@@ -111,37 +111,44 @@ app.post("/login", async (req, res) => {
 });
 
 
-//post a review for a city
+
+
+app.get("/logout", async (req, res) => {
+    try {
+        res.render("auth", {email:"", logout:true });
+    } catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+
 app.post('/api/reviews', async (req, res) => {
     try {
-      const { username, city, rating, description } = req.body;
-  
-      // Find the user by username
-      const user = await userModel.findOne({ username });
-  
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      // Create a new review
-      const newReview = new ReviewModel({
-        city,
-        rating,
-        description,
-      });
-  
-      // Add the review to the user's cityReviews array
-      user.cityReviews.push(newReview);
-  
-      // Save the updated user document
-      await user.save();
-  
-      res.status(201).json({ message: 'Review added successfully' });
+        // Extract data from the request body
+        const { island, rating, description } = req.body;
+        console.log(island);
+        console.log(rating);
+        console.log(description);
+
+        // Create a new review instance
+        const newReview = new reviewModel({
+            city: island, 
+            rating,
+            description,
+        });
+
+        // Save the review to the database
+        await newReview.save();
+
+        res.render("writetestimony", { success: 'success' })
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
-  });
+});
+
   app.get('/api/:username', async (req, res) => {
     try {
       const { username } = req.params;
@@ -159,24 +166,56 @@ app.post('/api/reviews', async (req, res) => {
       res.status(500).json({ message: 'Internal server error' });
     }
   });
+  
+  
+  app.post('/choosetrip', async (req, res) => {
+    try {
+        console.log(req.body)
+        const { selectedIslands, email, status} = req.body;
+
+        // Find the user in the database based on the email
+        const user = await userModel.findOne({ email: email });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
 
 
+        // Create a new trip with the selected islands
+        const newTrip = new tripModel({
+            cities: selectedIslands,
+            status: status
+        });
 
+        // Add the new trip to the user's trips array
+        user.trips = user.trips || [];
+        user.trips.push(newTrip);
 
+        // Save the updated user document in the database
+        await user.save();
 
-
-app.post("/complete",async(req,res)=>{
-    const data = {
-        startDate: req.body.startDate,
-        endDate: req.body.endDate,
+        res.json({ message: 'Trip saved successfully.' });
+    } catch (error) {
+        console.error('Error saving trip:', error);
+        res.status(500).json({ error: 'Error saving trip.' });
     }
+});
 
-    const newTrip = await tripModel.create(data);
-    console.log(newTrip);
-    res.render("completetrip", { success: 'success' })
-})
 
- 
+app.post('/viewtrip', async (req, res) => {
+    try {
+        const userEmail = req.body.email;
+        const userData = await userModel.findOne({ email: userEmail });
+        if (!userData) {
+            return res.status(404).send('Utente non trovato');
+        }
+        res.render('viewtrip', { userData });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Errore del server');
+    }
+});
+
 // Define Port for Application
 const port = 2323;
 app.listen(port, () => {
